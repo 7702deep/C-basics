50c448cd60f5723fc2a589c6c502290c6f3e76cd
7702deep <36358360+7702deep@users.noreply.github.com>
2019-07-22 15:56:24 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -2,23 +2,23 @@
 
 int main()
 {
-  int n,i,j,temp=0;
-  int a[100];
+  int n,i,j,c=0;
+  int t[100];
   scanf("%d",&n);
   for(i=0;i<n;i++)
   {
-      scanf("%d",&a[i]);
+      scanf("%d",&t[i]);
   }
   for(i=0;i<n;i++)
   {
       for(j=i+1;j<n;j++)
       {
           
-          if(a[i]<a[j])
+          if(a[i]<t[j])
           {
-              temp=a[i];
-              a[i]=a[j];
-              a[j]=temp;
+              c=t[i];
+              t[i]=t[j];
+              t[j]=c;
           }
           
           
@@ -28,7 +28,7 @@ int main()
   }
   for(i=0;i<n;i++)
   {
-      printf("%d",a[i]);
+      printf("%d",t[i]);
   }
   
     return 0;
